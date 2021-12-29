1e2ed3c3fee55ced23283b69ae4073e5b22662da
matt <mnunemacher@optonline.net>
2021-12-29 16:52:11 -0700

    format monetary values

--- src/components/CalculatorTotals.jsx
+++ src/components/CalculatorTotals.jsx
@@ -1,11 +1,23 @@
-import React from "react"
+import React from 'react'
 
-export const CalculatorTotals = ({totalBarRevenue, totalCommission}) => {
+export const CalculatorTotals = ({ totalBarRevenue, totalCommission }) => {
+    const formatTotal = (total) => {
+        if (isNaN(total)) total = 0.0
+        total = total.toFixed(2)
+
+        const [dollars, cents] = total.split('.')
+        return `$${dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}.${cents}`
+    }
+
     return (
-        <div className="sqs-block html-block sqs-block-html">
-            <div className="sqs-block-content">
-                <p style={{textAlign: 'center', whiteSpace: 'pre-wrap'}}>{`Total Bar Revenue: $${totalBarRevenue}`}</p>
-                <p style={{textAlign: 'center', whiteSpace: 'pre-wrap'}}>{`Commission: $${totalCommission}`}</p>
+        <div className='sqs-block html-block sqs-block-html'>
+            <div className='sqs-block-content'>
+                <p style={{ textAlign: 'center', whiteSpace: 'pre-wrap' }}>
+                    {`Total Bar Revenue: ${formatTotal(totalBarRevenue)}`}
+                </p>
+                <p style={{ textAlign: 'center', whiteSpace: 'pre-wrap' }}>
+                    {`Commission: ${formatTotal(totalCommission)}`}
+                </p>
             </div>
         </div>
     )
